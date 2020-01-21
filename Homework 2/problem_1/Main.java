327699a06453fc488528e59959d53d7c962ca3b7
Saravanan Sonia <srsonia98@yahoo.com.sg>
2020-01-21 00:43:17 -0800

    Create Main.java

---
+++ Homework 2/problem_1/Main.java
@@ -0,0 +1,17 @@
+/*
+CSS143B homework 2, problem 1
+by [your name]
+ */
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("testColorSort " + (Tests.testColorSort() ? "PASSED" : "FAILED"));
+    }
+
+    public static void colorSort(int[] nums) {
+        // homework
+        return; // place holder
+    }
+}
