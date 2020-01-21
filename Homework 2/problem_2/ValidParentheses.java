5c0587601ddf88c31299643370a6d97395157bcc
Saravanan Sonia <srsonia98@yahoo.com.sg>
2020-01-21 13:43:56 -0800

    Update ValidParentheses.java

--- Homework 2/problem_2/ValidParentheses.java
+++ Homework 2/problem_2/ValidParentheses.java
@@ -1,7 +1,29 @@
 public class ValidParentheses {
     public static boolean isValid(String str) {
-        // homework
-        return false; // place holder
+        Map<Character, Character> parenthesesMapping = new HashMap<>();
+        parenthesesMapping.put('(', ')');
+        parenthesesMapping.put('{', '}');
+        parenthesesMapping.put('[', ']');
+
+        Stack<Character> parentheses = new Stack<>();
+        for(int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            if(parenthesesMapping.containsKey(c)) {
+                parentheses.push(c);    
+            } else {
+                if(parentheses.isEmpty()) {
+                    return false;
+                }
+                char target = parentheses.pop();
+                if(!parenthesesMapping.containsKey(target) || parenthesesMapping.get(target) != c) {
+                    return false;
+                }
+            }
+        }
+        if(!parentheses.isEmpty()) {
+            return false;
+        }
+        return true;
     }
 
     public static boolean testIsValid() {
