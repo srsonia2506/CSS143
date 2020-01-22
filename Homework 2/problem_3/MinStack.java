18b12605466e7bbc322aef6e19b8b4efd689c2b2
Saravanan Sonia <srsonia98@yahoo.com.sg>
2020-01-22 15:16:14 -0800

    Update MinStack.java

--- Homework 2/problem_3/MinStack.java
+++ Homework 2/problem_3/MinStack.java
@@ -3,6 +3,28 @@ public class MinStack extends ArrayStack {
     public MinStack(int size) {
         super(size);
     }
+    
+    @Override
+    public boolean pop() {
+	if (data.isEmpty()){
+	    return false;
+    } else { 
+	    System.arraycopy(data, 0, data, 0, numElements-1);
+	    numElements--;
+            return true; 
+        } 
+    }
+
+    @Override
+    public boolean push(int val) {
+        if (data.isFull()){ 
+            return false; 
+        }else{ 
+            data[numElements] = val; 
+	    numElements++;	
+            return true; 
+        } 
+    }
 
     // add your comments for your design here
     public StackElement getMin() {
