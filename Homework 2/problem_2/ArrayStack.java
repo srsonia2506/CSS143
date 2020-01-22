41a582f6ef89a5f01d5cd9b2c3de4565ee7b9525
Saravanan Sonia <srsonia98@yahoo.com.sg>
2020-01-22 09:35:25 -0800

    Update ArrayStack.java

--- Homework 2/problem_2/ArrayStack.java
+++ Homework 2/problem_2/ArrayStack.java
@@ -1,43 +1,49 @@
 public class ArrayStack implements Stack {
 
+	private int numElements;
     private int size;
 	private int[] data;
-    	int top;  
 
-    private ArrayStack(){
-		this(Integer.MAX_VALUE - 8);
-		this.top = -1; 
+    private ArrayStack(){ 
 	}
 
     public ArrayStack(int size) {
        this.size=size;
 	   data= new int[size];
     }
+	
+	public boolean isEmpty() {
+		return (numElements <= 0);
+    }
+	
+	public boolean isFull() {
+		return (numElements >= size-1);
+    }
 
     @Override
     public boolean pop() {
-        if (top < 0) { 
-            System.out.println("Stack Underflow"); 
-            return 0; 
+		if (data.isEmpty()||data.isFull()){
+			return false;
         } else { 
-            int x = a[top--]; 
-            return x; 
+			//int val=data[size--];
+			numElements--;
+            return true; 
         } 
     }
 
     @Override
     public boolean push(int val) {
-        if (top >= (MAX - 1)){ 
+        if (data.isFull()){ 
             return false; 
         }else{ 
-            a[++top] = val; 
+            a[++numElements] = val; 
             return true; 
         } 
     }
 
     @Override
     public StackElement peek() {
-        if (top < 0) { 
+        if (data.isEmpty() < 0) { 
             return new StackElement(0, false); ; 
         } else { 
             return new StackElement(a[top], true); 
