77219b3cc85cdfb899392bbeac58810faee84df3
Saravanan Sonia <srsonia98@yahoo.com.sg>
2020-01-22 15:12:17 -0800

    Update ArrayStack.java

--- Homework 2/problem_3/ArrayStack.java
+++ Homework 2/problem_3/ArrayStack.java
@@ -1,57 +1,55 @@
+import java.lang.*;
+
 public class ArrayStack implements Stack {
 
-    private int data[];
+    private int numElements=0;
     private int size;
-	Integer minElement; 
-	Stack<Integer> s; 
+    private int[] data;
 
-    private ArrayStack(){
-		this(Integer.MAX_VALUE - 8);
-		this.top = -1; 
-	}
+    private ArrayStack(){}
 
     public ArrayStack(int size) {
        this.size=size;
-	   data= new int[size];
-	   s = new Stack<Integer>();
+       data= new int[size];
+    }
+	
+    public boolean isEmpty() {
+	return (numElements <= 0);
+    }
+	
+    public boolean isFull() {
+	return (numElements >= size-1);
     }
 
     @Override
     public boolean pop() {
-        if (s.isEmpty()) return false;
-		
-		Integer t=data[size--];
-
-        if (t < minElement){ 
-
-            return false; //PLACEHOLDERRRRRRRRR
-        } else {
-			//remove top element
-            return true;
-		}
+	if (data.isEmpty()){
+	    return false;
+    } else { 
+	    System.arraycopy(data, 0, data, 0, numElements-1);
+	    numElements--;
+            return true; 
+        } 
     }
 
     @Override
     public boolean push(int val) {
-        if (size >= Integer.MAX_VALUE - 8){ 
+        if (data.isFull()){ 
             return false; 
-        }
-		
-		if (s.isEmpty()||val < minElement){ 
-            minElement = val; 
-        }
-		
-		s.push(val); 
-		return true;
+        }else{ 
+            data[numElements] = val; 
+	    numElements++;	
+            return true; 
+        } 
     }
 
     @Override
     public StackElement peek() {
-        if (s.isEmpty())return new StackElement(0, false); 
-
-        if (t < minElement) return new StackElement(minElement, true); 
-        else return new StackElement(a[size-1], true); 
-
+        if (data.isEmpty() < 0) { 
+            return new StackElement(0, false); ; 
+        } else { 
+            return new StackElement(data[numElements-1], true); 
+        } 
     }
 
     @Override
@@ -59,6 +57,3 @@ public class ArrayStack implements Stack {
         return size;
     }
 }
-
-
- 
