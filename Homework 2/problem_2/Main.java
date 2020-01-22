13401e8e08a365cfe6f6bd1c2e20a2f602ef3721
Saravanan Sonia <srsonia98@yahoo.com.sg>
2020-01-22 14:23:30 -0800

    Update Main.java

--- Homework 2/problem_2/Main.java
+++ Homework 2/problem_2/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("testArrayStackEmptyQueue " + (ArrayStackTests.testArrayStackEmpty() ? "PASSED" : "FAILED"));
-        System.out.println("testArrayStackQueueOfSizeOne " + (ArrayStackTests.testArrayStackOfSizeOne() ? "PASSED" : "FAILED"));
-        System.out.println("testArrayStackQueueOfSizeTwo " + (ArrayStackTests.testArrayStackOfSizeTwo() ? "PASSED" : "FAILED"));
+        System.out.println("testArrayStackOfSizeOne " + (ArrayStackTests.testArrayStackOfSizeOne() ? "PASSED" : "FAILED"));
+        System.out.println("testArrayStackOfSizeTwo " + (ArrayStackTests.testArrayStackOfSizeTwo() ? "PASSED" : "FAILED"));
         System.out.println("testArrayStackOfSizeThree " + (ArrayStackTests.testArrayStackOfSizeThree() ? "PASSED" : "FAILED"));
         System.out.println("ValidParentheses.testIsValid " + (ValidParentheses.testIsValid() ? "PASSED" : "FAILED"));
     }
