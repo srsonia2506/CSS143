f8d125b01c087246f3bb6de3409a54ded7ea8f77
Saravanan Sonia <srsonia98@yahoo.com.sg>
2020-01-23 15:20:43 -0800

    Update MinStack.java

--- Homework 2/problem_3/MinStack.java
+++ Homework 2/problem_3/MinStack.java
@@ -1,34 +1,43 @@
-public class MinStack extends ArrayStack {
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class MinStack extends ArrayStack{
+
+    private Deque<Integer> stack = new LinkedList<Integer>();
+    private Deque<Integer> minimums = new LinkedList<Integer>();
 
     public MinStack(int size) {
         super(size);
     }
     
-    @Override
-    public boolean pop() {
-	if (size<=0){
-	    return false;
-    } else { 
-	    System.arraycopy(data, 0, data, 0, numElements-1);
-	    numElements--;
-            return true; 
-        } 
-    }
-
-    @Override
-    public boolean push(int val) {
-        if (s.isEmpty()||val < minElement){ 
-            minElement = val; 
+    @override
+    public boolean push(int x) {
+        if (size==data.length){ 
+            return false; 
+        } else if (x <= getMin()||stack.size==0) {
+            minimums.push(x);
         }
-		
-	s.push(val); 
-	return true;
+        stack.push(x);
+		return true;
+    }
+	
+	@override
+    public boolean pop() {
+	if (size == 0){
+		return false;
+	} 
+        int removed = stack.peek();
+		stack.pop();
+        if (removed == getMin()) {
+            minimums.pop();
+        }
+		return true;
     }
 
 
-    // add your comments for your design here
+    // Having a second stack and adds minimums to it. That way, even if minimum removed, the next top element in stack is the min
     public StackElement getMin() {
         if (s.isEmpty()) return new StackElement(0, false);
-        else return new StackElement(minElement, true);
+        else return new StackElement(minimums.peek(), true);
     }
 }
