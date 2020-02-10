baf5bc40eefb924c72bc241575b2fa866b0a0015
Saravanan Sonia <srsonia98@yahoo.com.sg>
2020-02-10 02:00:43 -0800

    Update Main.java

--- Homework 4/problem_2/Main.java
+++ Homework 4/problem_2/Main.java
@@ -45,15 +45,15 @@ public class Main {
 
     public static void bubbleSort(int data[]) {
       int n = data.length;
-        for (int i = 0; i < n-1; i++){
-            for (int j = 0; j < n-i-1; j++){
-                if (data[j] > data[j+1]){
-                  int temp = data[j];
-                  data[j] = data[j+1];
-                  data[j+1] = temp;
-                }
-			      }
-		    }
+      for (int i = 0; i < n-1; i++){
+        for (int j = 0; j < n-i-1; j++){
+          if (data[j] > data[j+1]){
+            int temp = data[j];
+            data[j] = data[j+1];
+            data[j+1] = temp;
+          }
+	}
+      }
     }
 
     public static void quickSort(int data[]) {
